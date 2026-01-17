695cc32007dd1ba3e9c9a4fbc60748c543e82b59
unlie9 <unlie.devops9@gmail.com>
2026-01-17 22:58:57 +0200

    sol

--- task.sql
+++ task.sql
@@ -1,11 +1,16 @@
 -- Use our database
-USE ShopDB; 
+USE ShopDB;
 
 -- Some data should be created outside the transaction (here)
+-- Create a new order (can be outside transaction as it's independent)
+INSERT INTO Orders (CustomerID, Date) VALUES (1, '2023-01-01');
 
--- Start the transaction 
-START TRANSACTION; 
+-- Start the transaction
+START TRANSACTION;
 
--- And some data should be created inside the transaction 
+-- And some data should be created inside the transaction
+-- Add order item and update warehouse amount atomically
+INSERT INTO OrderItems (OrderID, ProductID, Count) VALUES (1, 1, 1);
+UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
 
 COMMIT; 
