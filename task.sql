0591d6c5b8d34086370c6e97c03088decbd728ac
unlie9 <unlie.devops9@gmail.com>
2026-01-18 01:46:42 +0200

    sol3

--- task.sql
+++ task.sql
@@ -1,16 +1,12 @@
--- Use our database
 USE ShopDB;
 
--- Start the transaction
-START TRANSACTION;
 
--- Create a new order
 INSERT INTO Orders (CustomerID, Date) VALUES (1, '2023-01-01');
 
--- Add order item using LAST_INSERT_ID() to get the order ID
+START TRANSACTION;
+
 INSERT INTO OrderItems (OrderID, ProductID, Count) VALUES (LAST_INSERT_ID(), 1, 1);
 
--- Update warehouse amount
 UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
 
 COMMIT; 
